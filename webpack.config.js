fa48dccb55c37cec62c8a1240ce0a53b17c7b4d6
Largobox <largobox@yandex.ru>
2023-12-27 22:31:53 +0300

    Added file with constants

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -13,10 +14,30 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.tsx$/,
+        use: ['@svgr/webpack'],
+      }
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './static/index.html',
       inject: false, 
     }),
   ],
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()]
+  },
 };
